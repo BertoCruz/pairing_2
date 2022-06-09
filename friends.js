5ac8b4c3bccfc18d64d1c34c28f67c8747ba8ef7
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-06-09 09:31:13 -0600

    Add new friend

--- friends.js
+++ friends.js
@@ -1,7 +1,8 @@
-var bestFriend = "kitty";
+var bestFriend = "phil";
+var veryGoodFriend = "thomas";
 
 if(bestFriend === "kitty") {
   console.log("my best friend is kitty.");
-} else if (bestFriend === "dani") {
-  console.log("I guess kitty isn't my best friend");
+} else if (veryGoodFriend === "thomas") {
+  console.log("I guess kitty isn't my best friend, but I have thomas!");
 }
